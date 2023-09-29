c0eb6018501e3d67d8973456d9e961aeee06aece
Hailey Tran <78192606+hhytt-9@users.noreply.github.com>
2023-09-28 19:50:23 -0700

    Add skele code

---
+++ Assets/Scripts/PlayerHealthSkele.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using TMPro;
+
+public class PlayerHealthSkele : MonoBehaviour
+{
+    // UI for Losing
+    // public GameObject ____________;
+    // Text for player health
+    // public TextMeshProUGUI ____________;
+
+    // Player health
+    // ____________
+    // private int ____________;
+
+    // Start is called before the first frame update
+    // void Start()
+    // {
+    //     // ____________
+    // }
+
+    // void OnTriggerEnter2D(Collider2D other) {
+    //     // ____________ -= ____________;
+    //     if (____________) {
+    //         // Debug.Log("You died! :(");
+    //         ____________.text = "Hp: 0";
+    //         ____________.SetActive(true);
+    //         Time.timeScale = 0;
+    //     } else {
+    //         ____________.text = "Hp: " + hp;
+    //     }
+    // }
+}
---
+++ Assets/Scripts/PlayerMovementSkele.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovementSkele : MonoBehaviour
+{
+    // Player's rigidbody2d
+    // ____________;
+
+    // Can the player jump?
+    // ____________;
+
+    // How fast the player can move
+    // ____________
+    // ____________;
+
+    // Start is called before the first frame update
+    // void Start()
+    // {
+    //     // Assigning the player variables
+    //     // ____________;
+    // }
+
+    // Update is called once per frame
+    // void Update()
+    // {
+    //     // Get player input moving left and right
+    //     float x = ____________;
+    //     Get player input moving up and down
+    //     float y = ____________;
+
+    //     // Move the player!
+    //     Vector2 movementVector = new Vector2(____________, ____________);
+    //     ____________;
+
+    //     if (____________ && ____________) {
+    //         rb.AddForce(Vector2.up * ____________, ForceMode2D.Impulse);
+    //     }
+    // }
+
+    // void OnCollisionEnter2D(Collision2D other) {
+    //     if (____________) {
+    //         ____________;
+    //     }
+
+    //     if (____________) {
+    //         rb.AddForce(Vector2.up * ____________, ForceMode2D.Impulse);
+    //         Destroy(____________);
+    //     }
+    // }
+
+    // void OnCollisionStay2D(Collision2D other) {
+    //     if (____________) {
+    //         ____________;
+    //     }
+    // }
+
+    // void OnCollisionExit2D(Collision2D other) {
+    //     ____________
+    // }
+}
