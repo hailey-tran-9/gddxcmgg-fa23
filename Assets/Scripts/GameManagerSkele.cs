1cf2cf27ee3c57317f7541127102a7345c51bac9
hailey-tran-9 <78192606+hhytt-9@users.noreply.github.com>
2023-10-02 16:16:01 -0700

    Repo cleanup

---
+++ Assets/Scripts/GameManagerSkele.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManagerSkele : MonoBehaviour
+{
+    // // Enemies gameObject
+    // _________;
+    // // UI for winning
+    // _________;
+
+    // // Update is called once per frame
+    // void Update()
+    // {
+    //     if (_________ == 0) {
+    //         _________;
+    //     }
+    // }
+}
